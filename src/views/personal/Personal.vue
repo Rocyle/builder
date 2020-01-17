9624791715db22ea7498ede3a73df0865c9aa433
Rocyle <1369150705@qq.com>
2020-01-17 22:21:34 +0800

    加快进度

---
+++ src/views/personal/Personal.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="personal-content">
+    <div class="header">
+      <div class="title">会员中心</div>
+    </div>
+    <!-- 个人中心主题列表 -->
+    <div class="person-main">
+      <div class="usermsg menu-list">
+        <van-image
+          round
+          width="80px"
+          height="80px"
+          src="https://img.yzcdn.cn/vant/cat.jpeg"
+          class="head-pic"
+        />
+
+        <div class="user-text">
+          <p>
+            先生
+            <span>修改资料</span>
+          </p>
+          <p>编号：22309</p>
+        </div>
+      </div>
+      <div class="menu-list">
+        <div class="menu-item">
+          <div class="menu-icon">
+            <van-icon name="friends-o" />
+          </div>
+          <div class="menu-text">
+            我的招工
+            <van-icon name="arrow" />
+          </div>
+        </div>
+        <div class="menu-item">
+          <div class="menu-icon">
+            <van-icon name="friends-o" />
+          </div>
+          <div class="menu-text">
+            我的招工
+            <van-icon name="arrow" />
+          </div>
+        </div>
+      </div>
+      <div class="menu-list"></div>
+      <div class="menu-list"></div>
+      <div class="menu-list"></div>
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Footer
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="less" scoped >
+.personal-content {
+  background: #fefefe;
+  padding-bottom: 120px;
+  .header {
+    height: 260px;
+    background: linear-gradient(to bottom, #52ebe3 0%, #022566 100%);
+    border-radius: 0 0 200px 200px;
+    .title {
+      height: 100px;
+      color: #ffffff;
+      text-align: center;
+      font-size: 38px;
+      line-height: 100px;
+    }
+  }
+  .person-main {
+    padding: 0 30px;
+    margin-top: -130px;
+    .usermsg {
+      display: flex;
+      .head-pic {
+        margin: 0 20px;
+      }
+      .user-text {
+        flex: 1;
+      }
+    }
+    .menu-list {
+      margin-top: 14px;
+      padding: 30px 0 20px;
+      background: #fff;
+      border-radius: 10px;
+      box-shadow: 2px 2px 5px 5px #ccc;
+      .menu-item {
+        display: flex;
+        .menu-icon {
+          width: 50px;
+        }
+        .menu-text {
+          flex: 1;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/sendRecruit/SendRecruit.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="send-recruit-content">
+    <div class="header">
+      <div class="title">发布招工信息</div>
+    </div>
+    <div class="send-recruit-form">
+      <h4>请完善以下的信息</h4>
+      <div class="recruit-form">
+        <van-field v-model="text" label="标题" placeholder="请输入标题" />
+        <van-field v-model="text" label="所需工种" placeholder="请输入所需工种" />
+        <van-field v-model="text" label="地区" placeholder="请输入标题" />
+        <van-field v-model="text" label="详细地址" placeholder="请输入详细地址" />
+        <van-field v-model="text" label="联系人" placeholder="请输入标题" />
+        <van-field v-model="tel" type="tel" label="手机号" />
+        <van-field
+          v-model="message"
+          rows="2"
+          autosize
+          label="详情"
+          type="textarea"
+          maxlength="500"
+          placeholder="请输入详情"
+          show-word-limit
+        />
+      </div>
+      <div class="upload-img">
+        <h4>
+          上传图片
+          <span>(可以不上传)</span>
+        </h4>
+        <van-uploader v-model="fileList" multiple />
+      </div>
+    </div>
+    <van-button type="info" class="confirm-btn">确认</van-button>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Footer
+  },
+  data() {
+    return {
+      value: "",
+      message: "",
+      length: "",
+      text: "",
+      tel: "",
+      fileList: [
+        { url: "https://img.yzcdn.cn/vant/leaf.jpg" }
+        // Uploader 根据文件后缀来判断是否为图片文件
+        // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
+        // { url: "https://cloud-image", isImage: true }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="less"  scoped >
+@deep: ~">>>";
+.send-recruit-content {
+  background: #fefefe;
+  padding-bottom: 140px;
+  .header {
+    height: 200px;
+    background: linear-gradient(to bottom, #52ebe3 0%, #022566 100%);
+    .title {
+      height: 100px;
+      color: #ffffff;
+      text-align: center;
+      font-size: 38px;
+      line-height: 100px;
+    }
+  }
+  .send-recruit-form {
+    margin-top: -70px;
+    margin-bottom: 40px;
+    padding: 0 30px;
+    & > h4 {
+      padding: 40px 0;
+      color: #333;
+      text-align: center;
+      font-size: 38px;
+      font-weight: normal;
+      background: #fff;
+      border-radius: 6px;
+      box-shadow: 2px 2px 5px 5px #ccc;
+    }
+    .recruit-form {
+      padding: 20px 0;
+      border-radius: 6px;
+      margin-top: 14px;
+      box-shadow: 2px 2px 5px 5px #ccc;
+      @{deep}.van-field__label {
+        font-size: 16px;
+        font-weight: bold;
+      }
+    }
+    .upload-img {
+      border-radius: 6px;
+      margin-top: 14px;
+      box-shadow: 2px 2px 5px 5px #ccc;
+      padding: 20px 0;
+      h4 {
+        font-size: 26px;
+        text-align: center;
+        font-weight: normal;
+        line-height: 40px;
+        span {
+          color: #f00;
+        }
+      }
+      .van-uploader {
+        padding-top: 5px;
+      }
+    }
+  }
+  .confirm-btn{
+      display: block;
+      margin-top: 40px;
+      width: 400px;
+    //   height: 60px;
+      line-height: 80px;
+      text-align: center;
+      color: #fff;
+      font-size: 28px;
+      font-weight: bold;
+      margin: 0 auto;
+  }
+}
+</style>
